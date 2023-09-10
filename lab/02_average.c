8586d3e82f40c197e2b5e49b6a33ef2619bab482
Geroski <vlada.geroski@gmail.com>
2023-09-10 17:49:18 +0200

    Exercise for Chapter 2

---
+++ lab/02_average.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+  int i;
+  float val, sum;
+  i = 0;
+  sum = 0;
+  while(i != 5) {
+  	scanf("%f", &val);
+    sum += val;
+    i++;
+  }
+  printf("The total is: %.1f\n", sum);
+  printf("The average is: %.1f\n", sum / i);
+}
